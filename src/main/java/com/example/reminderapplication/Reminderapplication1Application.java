aa39a26b647b714ac80feae062087ebc1ddd89cd
Achmad Muharram <achmad.muharram@xtremax.id>
2020-01-02 11:57:16 +0700

    init commit

---
+++ src/main/java/com/example/reminderapplication/Reminderapplication1Application.java
@@ -0,0 +1,13 @@
+package com.example.reminderapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reminderapplication1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Reminderapplication1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reminderapplication/Reminderapplication1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reminderapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reminderapplication1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
